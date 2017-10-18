2380ad9d9b7a5d91ce7b733b2898f01a70788cf4
asmith <asmith@ccadllc.us>
2017-10-18 16:59:47 -0400

    tweaks

--- bitprice.py
+++ bitprice.py
@@ -4,15 +4,13 @@ from pprint import pprint
 import os
 import time
 
-while True:
-    time.sleep(3600)
-    url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
-    r = requests.get(url, headers={'Accept': 'application/json'})
-    data = r.json()
-    curr_price = data["bpi"]["USD"]["rate_float"]
-    pprint(curr_price)
-    if curr_price > 1000000:
-        os.system('say "your rich beotch"')
-    else:
-        os.system('say "your still not rich"')
+url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
+r = requests.get(url, headers={'Accept': 'application/json'})
+data = r.json()
+curr_price = data["bpi"]["USD"]["rate_float"]
+pprint(curr_price)
+if curr_price > 1000000:
+    os.system('say "your rich beotch"')
+else:
+    os.system('say "current bitcoin price is {} dollars"'.format(int(curr_price)))
 
